d2f40b4f2896ff40dd9e0792a18215023e432450
Ivan Dolgopolov <fertexop@yandex.ru>
2026-02-21 18:01:42 +0100

    Start 2 part of 1 lab

---
+++ Software_dev_tech/Converter_p1_p2/Conver_P_10.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Converter_p1_p2
+{
+    public class Conver_P_10
+    {
+        //Преобразовать цифру в число.
+        static double char_To_num(char ch) {        }
+        //Преобразовать строку в число
+        private static double convert(string P_num, int P, double weight) { }
+        //Преобразовать из с.сч. с основанием р 
+        //в с.сч. с основанием 10.
+        public static double dval(string P_num, int P) {         }
+    }
+}
